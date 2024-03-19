158aa9360d0d2f39ae7d1f2034c2f5c65666f0cf
Invitee <siancessp@gmail.com>
2024-03-19 12:43:31 +0530

    push

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -61,6 +61,7 @@ const insertuser = async (req, res) => {
             // Create JWT token for the newly registered user
             const token = await create_token(savedUser._id);
             console.log("Generated Token:", token);
+            console.log("Hii");
 
             const response = {
                 success: true,
