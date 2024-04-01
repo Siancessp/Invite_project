0e9fe0a64a1467130f602721bf4db160bc2a1469
Invitee <siancessp@gmail.com>
2024-04-01 19:50:08 +0530

    push

--- models/api/eventbookingModel.js
+++ models/api/eventbookingModel.js
@@ -10,7 +10,7 @@ const eventbookingSchema = new mongoose.Schema({
         required: true
     },
     eventBookingDates: {
-        type: Date,
+        type: String,
         required: true
     },
     numberofadult: {
