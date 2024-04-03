1ac6834c6a0ea4dd2a8b93e8a203d461a762fca1
Invitee <siancessp@gmail.com>
2024-04-03 16:35:17 +0530

    push

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const bcryptjs = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 
-
+const userRegister = require("../models/api/userregisterModel");
 
 const User = async (req, res) => {
     try {
@@ -15,6 +15,20 @@ const User = async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 };
+
+const getallUsers = async (req, res) => {
+    try {
+        const existedUserDetails = await userRegister.find();
+        if (!existedUserDetails || existedUserDetails.length === 0) {
+            return res.status(404).json({ success: false, msg: 'User Details not found' });
+        }
+        res.render('userlist', { existedUserDetails });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
 
 module.exports = {
     User
