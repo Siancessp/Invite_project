10faf94e225f9737cfa76e4320ddbbd1fae5c603
Invitee <siancessp@gmail.com>
2024-05-03 15:52:00 +0530

    p

--- controllers/bookhistoryController.js
+++ controllers/bookhistoryController.js
@@ -17,11 +17,18 @@ const getbookinghistorybyUserid = async (req, res) => {
             return res.redirect(previousPage);
         }
 
-        const bookedEventIds = userbookingDetails.map(booking => booking.bookedevent_id);
-        const statusCodes = bookedEventIds.map(satuscode =>  satuscode.status_code);
-
-        console.log(statusCodes);
-
+        userbookingDetails.forEach(booking => {
+            const { bookedevent_id, status_code } = booking;
+            console.log(bookedevent_id);
+            // Use conditional statements to handle status codes separately
+            if (status_code === '1') {
+                console.log("Hii");
+            } else if (status_code === 'STATUS_CODE_2') {
+                // Handle status code 2
+            } else {
+                // Handle other status codes
+            }
+        });
         res.render('userbookinghistorylist', { userbookingDetails });
     } catch(error) {
         console.error(error.message);
